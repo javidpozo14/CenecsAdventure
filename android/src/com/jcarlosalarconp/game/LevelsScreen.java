c50e5664b7d88c75c50ab539c083a2dd6c206c7f
jcarlosalarconp <48281276+jcarlosalarconp@users.noreply.github.com>
2020-03-03 19:05:54 +0100

    Android Activity
    -MainActivity is finished
    -LevelsScreen is in progress

--- android/src/com/jcarlosalarconp/game/LevelsScreen.java
+++ android/src/com/jcarlosalarconp/game/LevelsScreen.java
@@ -5,16 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LevelsScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_levels_screen);
     }
 
-    public void goLevelsScreen(View view) {
-        Intent intent= new Intent(this, LevelsScreen.class);
+    public void play(View view) {
+        Intent intent= new Intent(this, AndroidLauncher.class);
         startActivity(intent);
     }
 }
